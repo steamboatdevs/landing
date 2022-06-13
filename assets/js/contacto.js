3110a44d9d521bc05924232015da717496aec671
aldiaza <alr.diaza@gmail.com>
2022-06-12 22:08:45 -0400

    mensaje de notificacion y redireccionamiento

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -4,6 +4,7 @@ const send = async () => {
     const email = d.querySelector('#email');
     const name = d.querySelector('#name');
     const message = d.querySelector('#message');
+    const notification = d.querySelector('.notification');
     const config = {
         method: 'POST',
         headers: {
@@ -15,15 +16,42 @@ const send = async () => {
             message: message.value,
         }),
     };
-    data = await fetch('https://steamboat-mailer.onrender.com/mailing', config);
+    // data = await fetch('https://steamboat-mailer.onrender.com/mailing', config);
+    data = await fetch('http://localhost:8080/mailing', config);
     res = await data.json();
+    if (data.status !== 200) {
+        notification.style.visibility = 'visible';
+        notification.style.opacity = 1;
+        notification.style.transition = '0.5s ease';
+        notification.className += ' error';
+        notification.innerHTML = `
+            <p class="textNotification">Hubo un error al enviar el mensaje</p>
+        `;
+    } else {
+        notification.style.visibility = 'visible';
+        notification.style.opacity = 1;
+        notification.style.transition = '0.5s ease';
+        notification.className += ' success';
+        notification.innerHTML = `
+            <p class="textNotification">${res.msg}</p>
+        `;
+    }
 
     email.value = '';
     name.value = '';
     message.value = '';
+
+    setTimeout(() => {
+        notification.className = 'notification';
+        notification.style.opacity = 0;
+        notification.style.transition = '0.5s ease';
+        notification.style.visibility = 'hidden';
+        notification.innerHTML = '';
+    }, 10000);
 };
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log('click');
     send();
 });
